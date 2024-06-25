4ad436e5abe19db23c51ddb8eb08952c6b5f2d47
ivoronline <ivoronline@gmail.com>
2024-06-25 13:50:27 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_jdbc_update/service/MyService.java
+++ src/main/java/com/ivoronline/springboot_jdbc_update/service/MyService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -23,8 +22,7 @@ public class MyService {
     Connection connection = dataSource.getConnection();
 
     //INSERT RECORDS (Use single quotes for Strings)
-    String    sql       = " UPDATE PERSON SET NAME = '"+newName+"' WHERE name = '"+oldName+"' AND age = "+age+";" +
-                          " UPDATE PERSON SET NAME = 'Bill New'    WHERE name = 'Bill'";
+    String    sql       = " UPDATE PERSON SET NAME = '"+newName+"' WHERE name = '"+oldName+"' AND age = "+age;
     Statement statement = connection.createStatement();
     int       success   = statement.executeUpdate(sql);
 
